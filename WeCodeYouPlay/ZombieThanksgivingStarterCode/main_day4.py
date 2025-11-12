6ce69290b77e2025b8c52d61467f4ae02815598e
Renne Castro <rcastro2@users.noreply.github.com>
2025-11-12 22:17:45 +0000

    adds Day 4 outcome

---
+++ WeCodeYouPlay/ZombieThanksgivingStarterCode/main_day4.py
@@ -0,0 +1,28 @@
+from gamelib import *
+
+game = Game( 800, 600, "Zombie Thanksgiving" )
+bk = Image("Zbk.jpg",game)
+bk.resizeTo( 800, 600 )
+
+zombie = Image("zombie.png", game)
+zombie.resizeBy(-85)
+zombie.setSpeed(2,60)
+
+turkey = Image("turkey.gif", game)
+turkey.resizeBy(-50)
+turkey.setSpeed(3,-45)
+
+crosshair = Image("crosshair.png", game)
+crosshair.resizeBy(-70)
+crosshair.draw()
+
+while not game.over:
+    game.processInput()
+
+    bk.draw()
+    zombie.move(True)
+    turkey.move(True)
+    crosshair.moveTo(mouse.x, mouse.y)
+    
+    game.update(30)
+game.quit()
