09cff3d6f05ab8f9e9f8529f236c603c8aefac27
Renne Castro <rcastro2@users.noreply.github.com>
2025-11-23 00:05:30 +0000

    adds main final outcome

---
+++ WeCodeYouPlay/ZombieThanksgivingStarterCode/main_final.py
@@ -0,0 +1,65 @@
+from gamelib import *
+
+game = Game( 800, 600, "Zombie Thanksgiving" )
+
+bk = Image("Zbk.jpg",game)
+bk.resizeTo(800,600)
+
+zombie = Image("zombie.png",game)
+zombie.resizeBy(-90)
+zombie.setSpeed(2,60)
+
+turkey = Image("turkey.gif",game)
+turkey.resizeBy(-65)
+turkey.setSpeed(3,-45)
+
+crosshair = Image("crosshair.png",game)
+crosshair.resizeBy(-85)
+
+win = Image("youwin.png",game)
+lose = Image("youlose.png",game)
+
+gun = Sound("Gun2.wav",0)
+gobble = Sound("TurkeyGobble.wav",1)
+die = Sound("ZombieDie.wav",2)
+
+while not game.over:
+    game.processInput()
+
+    bk.draw()
+    zombie.move(True)
+    turkey.move(True)
+
+    if mouse.LeftClick:
+      gun.play()
+
+    if zombie.collidedWith( turkey  ):
+      x = randint(  100, 700  )
+      y = randint(  100, 500  )
+      turkey.moveTo( x, y )
+      turkey.health -= 5
+      gobble.play()
+
+    if mouse.collidedWith( zombie  ) and mouse.LeftClick:
+      x = randint(  100, 700  )
+      y = randint(  100, 500  )
+      zombie.moveTo( x, y )
+      zombie.damage += 5
+      zombie.speed += 2
+      die.play()
+
+    if turkey.health <= 0:
+        game.over = True
+        lose.draw()
+
+    if zombie.damage >= 100:
+        game.over = True
+        win.draw()
+
+    crosshair.moveTo(mouse.x, mouse.y)
+    game.drawText( "Zombie Damage: " + str(zombie.damage), 10, 10)
+    game.drawText( "Turkey Health: " + str(turkey.health), 10, 40)
+ 
+    game.update(30)
+game.wait(K_q)
+game.quit()
