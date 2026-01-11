15f5d594461ad459048757ddb36b51d4b3b8417d
Renne Castro <rcastro2@users.noreply.github.com>
2026-01-11 22:30:43 +0000

    updates main starter for Delta Fighter

--- WeCodeYouPlay/DeltaFighterStarterCode/main_starter.py
+++ WeCodeYouPlay/DeltaFighterStarterCode/main_starter.py
@@ -1,11 +1,15 @@
 from gamelib import *
 
-#Game Program
 game = Game(800,600,"Delta Fighter")
+bk = Animation("images/field_5.png",5,game,1000,1000)
+game.setBackground(bk)
 
-#Game Loop
+hero = Animation("images/hero2.png",3,game,288 / 3, 96, 2)
+    
 while not game.over:
     game.processInput()
-       
+    game.scrollBackground("down",2)
+
+
     game.update(30)
 game.quit()
