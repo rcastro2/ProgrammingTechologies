57b918e91f1cafa60565da19e719453a4b0d9195
Renne Castro <rcastro2@users.noreply.github.com>
2026-05-18 17:54:01 +0000

    updates Microbit Lesson 2 Demonstration

---
+++ CodeYourDevice/Lesson2c.py
@@ -0,0 +1,6 @@
+from microbit import *
+
+while True:
+    display.show(Image.GHOST)
+
+    sleep(1000)
---
+++ CodeYourDevice/Lesson2d.py
@@ -0,0 +1,10 @@
+from microbit import *
+
+while True:
+    display.show(Image('00300:'
+                       '03630:'
+                       '36963:'
+                       '03630:'
+                       '00300'))
+
+    sleep(1000)
