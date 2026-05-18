687ecafb0aea7077abfa1f055f6aaf45a045a4a4
Renne Castro <rcastro2@users.noreply.github.com>
2026-05-18 17:50:07 +0000

    add Microbit Lesson 2 Demonstration

---
+++ CodeYourDevice/Lesson2a.py
@@ -0,0 +1,15 @@
+from microbit import *
+
+while True:
+    display.set_pixel(0,0,3)
+    display.set_pixel(1,1,6)
+    display.set_pixel(2,2,9)
+    display.set_pixel(3,3,6)
+    display.set_pixel(4,4,3)
+
+    display.set_pixel(4,0,3)
+    display.set_pixel(3,1,6)
+    display.set_pixel(2,2,9)
+    display.set_pixel(1,3,6)
+    display.set_pixel(0,4,3)
+
---
+++ CodeYourDevice/Lesson2b.py
@@ -0,0 +1,5 @@
+from microbit import *
+
+while True:
+    display.scroll("Renne Castro")
+    sleep(2000)
